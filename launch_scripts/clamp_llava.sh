a2476ba606b49576c888871ab03639ee0855dcf3
Piotr <piotr.teterwak@gmail.com>
2024-04-22 20:33:52 -0400

    update path

--- launch_scripts/clamp_llava.sh
+++ launch_scripts/clamp_llava.sh
@@ -10,8 +10,6 @@
 #SBATCH -o /fsx/youngkyun/clamp/logs/slurm/%x_%A.o
 #SBATCH -e /fsx/youngkyun/clamp/logs/slurm/%x_%A.e
 
-eval "$(/data/home/youngkyun/miniconda3/bin/conda shell.bash hook)"
-conda activate piotr
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export MASTER_PORT=57129
 
@@ -23,5 +21,5 @@ export NCCL_P2P_DISABLE=1
 
 cd /data/home/youngkyun/piotr2/clamp
 export PYTHONPATH="$PYTHONPATH:$PWD/src"
-srun --cpu_bind=v --accel-bind=gn python -u  src/training/main.py --train-data '/datasets01/img2dataset/laion-400m/data_partitioned/0/{00000..01066}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/1/{00000..01067}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/2/{00000..01014}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/3/{00000..01085}.tar::/fsx/pteterwak/data/imagenet-21k_wds/train/{0..399}/{000000..000003}.tar'  --train-num-samples 10000000 --dataset-type webdataset_double_tokenizer   --batch-size 64 --zeroshot-frequency 1  --precision amp --workers 5  --dataset-resampled --model clamp_llava  --gather-with-grad --epochs 6 --lr 0.0005 --wd 0.5 --warmup 1220 --eps 1e-08 --local-loss --lock-image   --pretrained openai   --wrap-caption-long-list --logs /fsx/youngkyun/clamp/logs/ --grad-checkpointing --grad-clip-norm 1.0 --distill-model ViT-L-14 --distill-pretrained datacomp_xl_s13b_b90k
+srun --cpu_bind=v --accel-bind=gn python -u  src/training/main.py --train-data '/fsx/youngkyun/data/cc3m/cc3m/{00000..00331}.tar'  --train-num-samples 10000000 --dataset-type webdataset_double_tokenizer   --batch-size 64 --zeroshot-frequency 1  --precision amp --workers 5  --dataset-resampled --model clamp_llava  --gather-with-grad --epochs 6 --lr 0.0005 --wd 0.5 --warmup 1220 --eps 1e-08 --local-loss --lock-image   --pretrained openai   --wrap-caption-long-list --logs /fsx/youngkyun/clamp/logs/ --grad-checkpointing --grad-clip-norm 1.0 --distill-model ViT-L-14 --distill-pretrained datacomp_xl_s13b_b90k
 
