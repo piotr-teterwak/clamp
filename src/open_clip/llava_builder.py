401b4315c3add0e4fad8de3b72dc6a54fc157d2a
Piotr <piotr.teterwak@gmail.com>
2024-05-03 13:11:49 -0400

    Multi gpu fix.

--- src/open_clip/llava_builder.py
+++ src/open_clip/llava_builder.py
@@ -24,10 +24,10 @@ from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, D
 
 
 def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", device="cuda", use_flash_attn=False, **kwargs):
-    kwargs = {"device_map": device_map, **kwargs}
+    #kwargs = {"device_map": device_map, **kwargs}
 
-    if device != "cuda":
-        kwargs['device_map'] = {"": device}
+    #if device != "cuda":
+    #    kwargs['device_map'] = {"": device}
 
     if load_8bit:
         kwargs['load_in_8bit'] = True
